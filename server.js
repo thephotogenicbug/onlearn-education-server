355a4ae8aa4f7e14c9545df60c51bd7f66132fe7
thephotogenicbug <navinkumarachari@gmail.com>
2025-07-02 20:54:14 +0530

    setup mongoDB Connection and use cookieParser

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
+connectDB();
+
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
 app.get("/", (req, res) => {
