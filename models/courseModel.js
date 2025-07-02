c0137e00b11e79fbc003eb448552cffea02815f7
thephotogenicbug <navinkumarachari@gmail.com>
2025-07-02 21:04:04 +0530

    create course and user Model

---
+++ models/courseModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const courseSchema = mongoose.Schema(
+  {
+    courseName: { type: String, required: true },
+    courseDesc: { type: String, required: true },
+    price: { type: Number, required: true },
+    Baseprice: { type: Number, required: true },
+    noOfStudents: { type: Number, required: true },
+    image: { type: String, required: true },
+    time: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const courseModel =
+  mongoose.models.courses || mongoose.model("courses", courseSchema);
+export default courseModel;
