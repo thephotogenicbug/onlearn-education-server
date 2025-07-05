3e5bd4add948fd6fd65c928e8b1337fc04369e91
thephotogenicbug <navinkumarachari@gmail.com>
2025-07-05 17:19:24 +0530

    create function and routes to update and delete course

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -34,3 +34,60 @@ export const newcourse = async (req, res) => {
     return res.json({ success: false, message: error.message });
   }
 };
+
+// @put - edit course
+export const updateCourse = async (req, res) => {
+  const { id } = req.params;
+  const { coursename, coursedesc, price, baseprice, no_of_students, image } =
+    req.body;
+
+  if (
+    !coursename ||
+    !coursedesc ||
+    !price ||
+    !baseprice ||
+    !no_of_students ||
+    !image
+  ) {
+    return res.json({ success: false, message: "missing course information" });
+  }
+
+  try {
+    const updateCourse = await courseModel.findByIdAndUpdate(id, {
+      courseName: coursename,
+      courseDesc: coursedesc,
+      price: price,
+      BasePrice: baseprice,
+      noOfStudents: no_of_students,
+      image: image,
+    });
+    return res.json({
+      success: true,
+      message: "course details updated successfully",
+      updateCourse,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+// @put - edit course
+export const deleteCourse = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleteCourse = await courseModel.findByIdAndDelete(id);
+    if (!deleteCourse) {
+      return res.json({
+        success: false,
+        message: "course data is invalid | missing",
+      });
+    }
+    return res.json({ success: true, message: "course deleted successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
--- routers/courseRouter.js
+++ routers/courseRouter.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { newcourse } from "../controllers/courseController.js";
+import {
+  deleteCourse,
+  newcourse,
+  updateCourse,
+} from "../controllers/courseController.js";
 
 const courseRouter = express.Router();
 
 courseRouter.post("/new-course", newcourse);
+courseRouter.put("/update-course/:id", updateCourse);
+courseRouter.delete("/delete-course/:id", deleteCourse);
 
 export default courseRouter;
